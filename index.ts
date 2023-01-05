e0cd676ce810eeb5e9a706576a11c447ba4d4804
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2023-01-05 10:17:06 +0800

    update

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { Flipside, Query, QueryResultSet } from "@flipsidecrypto/sdk";
+
+// Initialize `Flipside` with your API key
+const flipside = new Flipside(
+  "00dc2cb5-185f-4e83-8d58-8844ed171df4",
+  "https://api.flipsidecrypto.com"
+);
+
+// Create a query object for the `query.run` function to execute
+// with a page size of 100 rows, starting with page #1.
+const query: Query = {
+  sql: `
+    SELECT
+        to_asset,
+        COUNT(to_asset)
+    FROM thorchain.swaps
+    GROUP BY to_asset
+    ORDER BY COUNT(to_asset) DESC
+  `,
+  ttlMinutes: 10,
+  pageSize: 100,
+  pageNumber: 1,
+};
+
+// Send the `Query` to Flipside's query engine and await the results for Page #1
+const page1Results: QueryResultSet = await flipside.query.run(query);
+
+console.log(page1Results);
